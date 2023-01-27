675dc0ba68fc27633d9eebcbe47f3276b67fea66
Emo <emotupev11@gmail.com>
2023-01-28 00:49:26 +0200

    fix: bug

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   document.querySelector(".image")
   .addEventListener("click", (e) => {
-   e.target.style.transform  = "scale(2)"
+   document.querySelector(".image").style.transform  = "scale(2)"
   })
 
   const button = document.querySelector(".button");
